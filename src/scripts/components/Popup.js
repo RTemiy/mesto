dc16dee769dd2917aa42f07b2942bd9d5c41eaed
RTemiy <RTemiyGo@yandex.ru>
2023-05-24 14:56:21 +0300

    пр 8 правка

--- src/scripts/components/Popup.js
+++ src/scripts/components/Popup.js
@@ -6,16 +6,16 @@ export  default class Popup {
 
   open() {
     this._popup.classList.add('popup_opened');
-    document.addEventListener('keydown', this._handleEscClose);
+    document.addEventListener('keydown', this._handleEscClose.bind(this));
   }
 
   close() {
     this._popup.classList.remove('popup_opened');
-    document.removeEventListener('keydown', this._handleEscClose);
+    document.removeEventListener('keydown', this._handleEscClose.bind(this));
   }
 
   _handleEscClose = e => {
-    if (e.key === 'Escape') this.close.bind(this);
+    if (e.key === 'Escape') this.close();
   }
 
   setEventListeners() {
